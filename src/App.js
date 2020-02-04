3b4aad181f16c776020496bf77981e42e52fcffd
Jessica Wilson <jpwilson1293@gmail.com>
2020-02-03 22:30:25 -0600

    can get playlists for a user

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ import $ from 'jquery';
 import './App.css';
 
 const App = () => {
+
+  const [accessToken, setAccessToken] = React.useState();
+
+  const getStuff = () => {
+    $.ajax({
+      url: "https://api.spotify.com/v1/users/jwil1993/playlists",
+      headers: {Authorization: `Bearer ${accessToken}`},
+      success: response => {
+        console.log(response)
+      }
+    })
+
+  };
+
   React.useEffect(() => {
     $.ajax({
       url: "https://accounts.spotify.com/api/token",
@@ -12,7 +26,7 @@ const App = () => {
       headers: {"Authorization": `Basic ${btoa(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`)}`},
       data: {grant_type: "client_credentials"},
       success: response => {
-        console.log(response);
+        setAccessToken(response.access_token);
       },
       error: response => {
         console.log('ERROR', response)
@@ -24,17 +38,10 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button type="button" onClick={getStuff}>Get Stuff</button>
         <p>
           This is my neat music bot thing.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
